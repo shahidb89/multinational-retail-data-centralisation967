ed97ee220a4645a03c87e8476674219fbaacc8e9
shahidb89 <shahid_b89@hotmail.com>
2025-01-23 16:20:20 +0000

    Adds init_db_engine() method.

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,5 @@
 import yaml
+from sqlalchemy import create_engine
 
 class DatabaseConnector:
     def read_db_creds():
@@ -7,5 +8,18 @@ class DatabaseConnector:
         return db_creds_dict
     
 
-
-
+    def init_db_engine():
+        db_cred_dict =  DatabaseConnector.read_db_creds()
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        ENDPOINT = db_cred_dict['RDS_HOST']
+        USER = db_cred_dict['RDS_USER']
+        PASSWORD = db_cred_dict['RDS_PASSWORD']
+        PORT = db_cred_dict['RDS_PORT']
+        DATABASE = db_cred_dict['RDS_DATABASE']
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+        engine.connect()
+        return engine
+    
+if __name__ == '__main__':
+    d=DatabaseConnector.init_db_engine()
