7418491f580d1568da6e582dbc678b1d5b18e6b1
shahidb89 <shahid_b89@hotmail.com>
2025-01-25 11:33:01 +0000

    CORRECTION: read_rds_table() method name corrrected to list_rds_tables(), because the added function lists the RDS table but does not read them.

--- data_extraction.py
+++ data_extraction.py
@@ -1,13 +1,13 @@
 import database_utils as dbut
 from sqlalchemy import inspect
 class DataExtractor:
-    def read_rds_table():
+    def list_rds_tables():
         engine = dbut.DatabaseConnector.init_db_engine()
         inspector = inspect(engine)
         tables = inspector.get_table_names()
         return tables
 
-tables = DataExtractor.read_rds_table()
+tables = DataExtractor.list_rds_tables()
 
 for t in tables:
     print (t)
