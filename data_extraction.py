7acf0ace2ec3f1cd22f834eef89539c936b620a0
shahidb89 <shahid_b89@hotmail.com>
2025-01-26 14:23:17 +0000

    Adds read_rds_table() method.

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,5 @@
 import database_utils as dbut
+import pandas as pd
 from sqlalchemy import inspect
 class DataExtractor:
     def list_rds_tables():
@@ -6,8 +7,22 @@ class DataExtractor:
         inspector = inspect(engine)
         tables = inspector.get_table_names()
         return tables
+    
+    def read_rds_table(table):
+        engine = dbut.DatabaseConnector.init_db_engine()
+        df = pd.read_sql_table(table, engine)
+        return df
 
-tables = DataExtractor.list_rds_tables()
 
-for t in tables:
-    print (t)
+user_df = DataExtractor.read_rds_table("legacy_users")
+
+print (user_df.head())
+# tables = DataExtractor.list_rds_tables()
+
+# for t in tables:
+#     print (t)
+
+# if __name__ == "__main__":
+
+#     user_table = DataExtractor.read_rds_table("orders_table")
+#     print(user_table.head(5))
