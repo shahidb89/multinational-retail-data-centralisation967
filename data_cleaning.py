cf8f55a2a768029375d765c33601ac4263f795e5
shahidb89 <shahid_b89@hotmail.com>
2025-01-26 15:58:34 +0000

    Updates and optimises clean_user_data() method.

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,12 +4,13 @@ import data_extraction as dtex
 class DataCleaning:
     def clean_user_data():
         user_df = dtex.user_df
-        user_df.replace(['NULL', 'null', 'NaN'], np.nan, inplace=True)
-        user_df.dropna()
-        user_df["join_date"] = pd.to_datetime(user_df["join_date"], 
-                                              errors='coerce')
+        user_df.set_index("index", inplace=True)
+        user_df.replace(['NULL'], np.nan, inplace=True)
+        user_df["join_date"] = pd.to_datetime(user_df["join_date"],
+                                               errors='coerce')
+        user_df = user_df.dropna()
         return user_df
 
 if __name__ == "__main__":
     df = DataCleaning.clean_user_data()
-    print(df.head(5))
+    print(df)
