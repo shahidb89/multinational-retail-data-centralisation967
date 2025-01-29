fe05707b3c2161240b3408c2563c6f76a7013de6
shahidb89 <shahid_b89@hotmail.com>
2025-01-29 11:52:31 +0000

    Adds retrieve_pdf_data() method successfully.

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,7 @@
 import database_utils as dbut
 import pandas as pd
 from sqlalchemy import inspect
+from tabula import read_pdf
 class DataExtractor:
     def list_rds_tables():
         engine = dbut.DatabaseConnector.init_db_engine()
@@ -13,16 +14,22 @@ class DataExtractor:
         df = pd.read_sql_table(table, engine)
         return df
 
+    def retrieve_pdf_data(link):
+        dfs = read_pdf(link, pages="all", multiple_tables=True)
+        combined_df = pd.concat(dfs, ignore_index=True)
+        return combined_df
+
 
 user_df = DataExtractor.read_rds_table("legacy_users")
 
-print (user_df.head())
+
+
 # tables = DataExtractor.list_rds_tables()
 
 # for t in tables:
 #     print (t)
 
-# if __name__ == "__main__":
-
-#     user_table = DataExtractor.read_rds_table("orders_table")
-#     print(user_table.head(5))
+if __name__ == "__main__":
+    link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+    card_table = DataExtractor.retrieve_pdf_data(link)
+    print(card_table)
